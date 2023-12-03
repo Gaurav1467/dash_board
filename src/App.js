dcc8aa29d4dc06cb0f42dcdc298d380469fc6df8
Gaurav1467 <19uec146@lnmiit.ac.in>
2023-12-03 23:36:32 +0530

    fc

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import { useEffect } from 'react';
 
 function App() {
+
+  const [rows, setRows] = useState([]) 
+
+  
+  const fetchRows = async () => {
+
+
+
+    const url = "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json";
+    const data = await fetch(url);
+
+    const dataJson = await data.json();
+
+    return dataJson;
+    
+  }
+  
+  useEffect( () => {
+
+  fetchRows().then(dataJson => setRows(dataJson));    
+
+  }, [])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Home rows = {rows} setRows = {setRows} />
+    </>
   );
 }
 
